d17eff992a69f872261bb36192160fc28204513c
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-05-13 10:47:47 +0200

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -22,19 +22,19 @@ def main():
         if (eags == "E"):
             amount = input(str("How many passwords should be generated? "))
             for i in range(1,int(amount) + 1):
-                print(colors.MAGENTA + "Password " + str(i) + ": " + random.choice(wordlist) + random.choice(ciphersl + colors.RESET))
+                print("Password " + str(i) + ": " + colors.MAGENTA + random.choice(wordlist) + random.choice(ciphersl + colors.RESET))
         elif (eags == "A"):
             amount = input(str("How many passwords should be generated? "))
             for i in range(1,int(amount) + 1):
-                print(colors.MAGENTA + "Password " + str(i) + ": " + random.choice(wordlist) + random.choice(chl) + random.choice(ciphersl) + colors.RESET)
+                print("Password " + str(i) + ": " + colors.MAGENTA + random.choice(wordlist) + random.choice(chl) + random.choice(ciphersl) + colors.RESET)
         elif (eags == "G"):
             amount = input(str("How many passwords should be generated? ") + colors.RESET)
             for i in range(1,int(amount) + 1):
-                print(colors.MAGENTA + "Password " + str(i) + ": " + random.choice(ciphersl) + random.choice(wordlist) + random.choice(chl) + random.choice(ciphersl) + colors.RESET)
+                print("Password " + str(i) + ": " + colors.MAGENTA + random.choice(ciphersl) + random.choice(wordlist) + random.choice(chl) + random.choice(ciphersl) + colors.RESET)
         elif (eags == "S"):
             amount = input(str("How many passwords should be generated? "))
             for i in range(1,int(amount) + 1):
-                print(colors.MAGENTA + "Password " + str(i) + ": " + random.choice(chl) + random.choice(wordlist) + random.choice(chl) + random.choice(ciphersl) + random.choice(wordlist) + random.choice(chl) + colors.RESET)
+                print(colors.MAGENTA + "Password " + str(i) + ": " + colors.MAGENTA + random.choice(chl) + random.choice(wordlist) + random.choice(chl) + random.choice(ciphersl) + random.choice(wordlist) + random.choice(chl) + colors.RESET)
         elif (eags == "Q"):
             print(colors.RED + "Quitting... ")
             stop = True
